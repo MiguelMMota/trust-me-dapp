d465ecdb3eb85377316ac2f5cc3eaa5c9bae8360
Miguel Mota <mota.m.miguel@gmail.com>
2025-11-11 22:06:45 +0000

    Add anvil local network

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,16 +1,13 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, sepolia, polygon, optimism, arbitrum, base } from 'wagmi/chains';
+import { mainnet, sepolia, anvil } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'TrustMe dApp',
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'YOUR_PROJECT_ID',
   chains: [
+    anvil,
     mainnet,
     sepolia,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
   ],
   ssr: true,
 });
